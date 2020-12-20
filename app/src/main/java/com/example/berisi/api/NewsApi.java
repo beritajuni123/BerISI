3e2024a83279ee8010410bf75bb00cd43dd86e2d
beritajuni123 <junijuniteen@gmail.com>
2020-12-20 22:52:04 +0700

    wahhh dah pusing aku aku nyerah deh

--- app/src/main/java/com/example/berisi/api/NewsApi.java
+++ app/src/main/java/com/example/berisi/api/NewsApi.java
@@ -1,10 +1,10 @@
 package com.example.berisi.api;
 
 public class NewsApi {
-    public static String GET_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=id&apiKey=f3aa09e2f1d7494e9fded68f30d4e4c1";
-    public static String GET_CATEGORY_SPORTS = "https://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=f3aa09e2f1d7494e9fded68f30d4e4c1";
-    public static String GET_CATEGORY_TECHNOLOGY = "https://newsapi.org/v2/top-headlines?country=id&category=technology&apiKey=f3aa09e2f1d7494e9fded68f30d4e4c1";
-    public static String GET_CATEGORY_BUSINESS = "https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=f3aa09e2f1d7494e9fded68f30d4e4c1";
-    public static String GET_CATEGORY_HEALTH = "https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=f3aa09e2f1d7494e9fded68f30d4e4c1";
-    public static String GET_CATEGORY_ENTERTAINMENT = "https://newsapi.org/v2/top-headlines?country=id&category=entertainment&apiKey=f3aa09e2f1d7494e9fded68f30d4e4c1";
+    public static String GET_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=id&apiKey=4b4b20d72cb24004bc0b212827a76859";
+    public static String GET_CATEGORY_SPORTS = "https://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=4b4b20d72cb24004bc0b212827a76859";
+    public static String GET_CATEGORY_TECHNOLOGY = "https://newsapi.org/v2/top-headlines?country=id&category=technology&apiKey=4b4b20d72cb24004bc0b212827a76859";
+    public static String GET_CATEGORY_BUSINESS = "https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=4b4b20d72cb24004bc0b212827a76859";
+    public static String GET_CATEGORY_HEALTH = "https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=4b4b20d72cb24004bc0b212827a76859";
+    public static String GET_CATEGORY_ENTERTAINMENT = "https://newsapi.org/v2/top-headlines?country=id&category=entertainment&apiKey=4b4b20d72cb24004bc0b212827a76859";
 }
